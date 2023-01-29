aec809cd656165e15ba09fac6d030f06910db575
sebastianboari <luciosebastianboari@gmail.com>
2023-01-29 03:08:35 -0300

    Funcionalidades / Logica de configUser e inicio de sesion mejoradas. Estilos / Estilos generales mejorados.

--- js/components/dom.js
+++ js/components/dom.js
@@ -1,5 +1,3 @@
-
-
 // Body
 const body = document.getElementById('body');
 // Footer
@@ -20,37 +18,21 @@ accountPanelBtnConfig.style.display = 'none';
 // Configuration
 const sectionConfig = document.getElementById('configuration');
 const profileImgOptions = document.querySelectorAll('.profileImgOptions');
-let userIconOnLocaleStorage = localStorage.getItem('userIcon');
 const configUserIcon = document.getElementById('configUserIcon');
-const userName = document.getElementById('userName');
-const userPhone = document.getElementById('userPhone');
-const userEmail = document.getElementById('userEmail');
-const userPassword = document.getElementById('userPassword');
+const userNameConfig = document.getElementById('userNameConfig');
+const userPhoneConfig = document.getElementById('userPhoneConfig');
+const userEmailConfig = document.getElementById('userEmailConfig');
+const userPasswordConfig = document.getElementById('userPasswordConfig');
+const configUserForm = document.getElementById('configUser__form');
 
-
-// Muestra la informacion del usuario en pantalla
-const userDataConfig = () =>{
-  userName.value = localStorage.getItem('userName');
-  userPhone.value = localStorage.getItem('userPhone');
-  userEmail.value = localStorage.getItem('userEmail');
-  userPassword.value = localStorage.getItem('userPassword');
+const userOnStorage = {
+  defaultIcon: './assets/icons/account-circle.svg',
+  userIcon: localStorage.getItem('userIcon'),
+  userName: localStorage.getItem('userName'),
+  userEmail: localStorage.getItem('userEmail'),
+  userPhone: localStorage.getItem('userPhone'),
+  userPassword: localStorage.getItem('userPassword')
 };
-
-
-// Activa la opcion de elegir la imagen de perfil
-const chooseUserIcon = () =>{
-  profileImgOptions.forEach(img => {
-    img.addEventListener('click', function(){
-      const src = this.getAttribute('src');
-      localStorage.setItem('userIcon', `${src}`);
-  
-      document.querySelectorAll('.userIcon').forEach(img => {
-        img.setAttribute('src', src);
-      });
-    });
-  });
-};
-
 
 // Verifica si la sesion esta activa
 const checkSession = () =>{
@@ -61,8 +43,39 @@ const checkSession = () =>{
   };
 };
 
+// Muestra la informacion del usuario en pantalla y permite editar los datos
+const userDataConfig = () =>{
+  // Muestro la informacion en pantalla
+  configUserIcon.setAttribute('src', `${userOnStorage.userIcon}`);
+  userNameConfig.value = userOnStorage.userName;
+  userPhoneConfig.value = userOnStorage.userPhone;
+  userEmailConfig.value = userOnStorage.userEmail;
+  userPasswordConfig.value = userOnStorage.userPassword;
 
-// Boton 'cuenta de usuario' en sesion ACTIVA
+  configUserForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    localStorage.setItem('userIcon', `${configUserIcon.getAttribute('src')}`) ;
+    localStorage.setItem('userName', `${userNameConfig.value}`);
+    localStorage.setItem('userPhone', `${userPhoneConfig.value}`); 
+    localStorage.setItem('userEmail', `${userEmailConfig.value}`);
+    localStorage.setItem('userPassword', `${userPasswordConfig.value}`);
+  });
+
+};
+
+// Activa la opcion de elegir la imagen de perfil
+const chooseUserIcon = () =>{
+  profileImgOptions.forEach(img => {
+    img.addEventListener('click', function(){
+      const src = this.getAttribute('src');
+      document.querySelectorAll('.userIcon').forEach(img => {
+        img.setAttribute('src', src);
+      });
+    });
+  });
+};
+
+// NAVEGACION DE USARIO EN SESION ACTIVA
 const btnAccountActiveSession = () => {
   // Boton LogOut
   const accountBtnLogOut = () =>{
@@ -71,6 +84,7 @@ const btnAccountActiveSession = () => {
       location.reload();
     });
   };
+
   // Boton Config
   const accountBtnConfig = () =>{
     accountPanelBtnConfig.addEventListener('click', () =>{
@@ -95,7 +109,7 @@ const btnAccountActiveSession = () => {
 };
 
 
-// Boton cuenta de usuario INACTIVO
+// NAVEGACION DE USARIO EN SESION INACTIVA
 const btnAccountNoActiveSession = () => {
   // Boton LogIn
   const accountBtnLogIn = () => {
@@ -104,38 +118,42 @@ const btnAccountNoActiveSession = () => {
       initLogIn();
     });
   };
-  
   btnAccount.addEventListener('click', () => {
     accountPanel.classList.toggle('display-none');
-
     // Muestra solo Log In
     accountPanelBtnLogIn.style.display = 'block';
     accountBtnLogIn();
-
     accountPanelBtnLogOut.style.display = 'none';
     accountPanelBtnConfig.style.display = 'none'; 
   });
 };
 
-
-
+// HEADER EN SESION ACTIVA
 const headerActiveSession = () => {
-  configUserIcon.setAttribute('src', userIconOnLocaleStorage);
-  headerUserIcon.setAttribute('src', userIconOnLocaleStorage);
+
   btnAccountActiveSession();
 };
+// HEADER EN SESION INACTIVA
 const headerNoActiveSession = () =>{
   btnAccountNoActiveSession();
 };
-
+// Inicializo Carrusel
 const swiper = new Swiper(".mySwiper", {
   autoplay: true,
 });
 
-// Funcion Inicializadora
+
+// FUNCION INICIALIZADORA DEL HEADER COMPLETO
 function headerInit(){ 
   header.style.display = 'block';
-  
+
+  //Verifico si ya hay un icono en localeStorage
+  if(userOnStorage.userIcon !== null || userOnStorage.userIcon !== undefined){
+    headerUserIcon.setAttribute('src', userOnStorage.userIcon);
+  } else {
+    headerUserIcon.setAttribute('src', userOnStorage.defaultIcon);
+  };
+
   if(checkSession() == true){
     headerActiveSession();
   } else {
@@ -143,6 +161,27 @@ function headerInit(){
   };
 };      
 
+// Mostrar header y footer
+const displayHeaderAndFooter = () => {
+  header.style.display = 'grid';
+  footer.style.display = 'grid';
+};
+
+// Esconder header y footer
+const hideHeaderAndFooter = () => {
+  header.style.display = 'none';
+  footer.style.display = 'none';
+};
+
+// Mostrar Configuracion
+const displayConfig = () =>{
+workflow.style.display = 'none';
+sectionHome.style.display = 'none';
+sectionConfig.style.display = 'grid';
+};
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
 // SCREENS
@@ -192,29 +231,32 @@ function homeInit(){
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 // WORKFLOW
 const workflow = document.getElementById('workflow');
 workflow.style.display = 'none';
+
+// Mostrar Workflow
+const displayWorkflow = () =>{
+  // Hide
+  sectionHome.style.display = 'none';
+  sectionSignUp.style.display = 'none';
+  sectionLogIn.style.display = 'none';
+  // Show
+  displayHeaderAndFooter();
+  workflow.style.display = 'grid';
+};
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -255,6 +297,20 @@ const btnSignInEvent = () => {
   });
 };
 
+// Mostrar SignUp
+const displaySignUp = () => {
+  // Hide
+  hideHeaderAndFooter();
+  sectionHome.style.display = 'none';
+  workflow.style.display = 'none';
+  sectionLogIn.style.display = 'none';
+  // Show
+  sectionSignUp.style.display = 'grid';
+  // Init logica y eventos
+  btnSignInEvent();
+  initSignUp();
+};
+
 
 
 // LOG IN
@@ -280,17 +336,6 @@ const btnSignUpEvent = () => {
   });
 };
 
-
-
-
-
-
-
-
-
-
-
-
 // Mostrar LogIn
 const displayLogIn = () =>{
     // Hide
@@ -305,55 +350,239 @@ const displayLogIn = () =>{
     initLogIn();
 };
 
-// Mostrar SignUp
-const displaySignUp = () => {
-    // Hide
-    hideHeaderAndFooter();
-    sectionHome.style.display = 'none';
-    workflow.style.display = 'none';
-    sectionLogIn.style.display = 'none';
-    // Show
-    sectionSignUp.style.display = 'grid';
-    // Init logica y eventos
-    btnSignInEvent();
-    initSignUp();
+
+
+
+
+
+
+
+
+
+// SIGN UP
+
+// Funciones de validacion:
+function validateEmail(email){
+  let re = /^[^@]+@[^@.]+\.[^@]{2,}$/;
+  if(re.test(String(email).toLowerCase())){
+      return true;
+  }else{
+      return false;
+  };
+};
+function validatePhone(phoneNumber){
+  if(phoneNumber.length > 9){
+      return true;
+  }else{
+      return false;
+  }
+};
+function validateName(name){
+let nameRegex = /^[a-zA-Z\s]*$/;
+  if(name.match(nameRegex) && name.length > 8) {
+      return true;
+  } else {
+      return false;
+  };
+};
+function validatePassword(password){
+  if (password.length < 8) {
+      return false;
+  } else {
+      return true;
+  }
+};
+function validateConfirmPassword(password, confirmPassword){
+  if (password == confirmPassword){
+      return true;
+  } else{
+      return false
+  }
 };
 
-// Mostrar Workflow
-const displayWorkflow = () =>{
-    // Hide
-    sectionHome.style.display = 'none';
-    sectionSignUp.style.display = 'none';
-    sectionLogIn.style.display = 'none';
-    // Show
-    displayHeaderAndFooter();
-    workflow.style.display = 'grid';
+// Manipulacion de estilos
+// Funcion de validacion con un solo parametro:
+function validateFieldStyles(value, label, icon, iconPath, validationFunction){
+  if(validationFunction(value) == false){
+      label.classList.add('error');
+      icon.src = `./assets/icons/${iconPath}-error.png`;
+  } else if(validationFunction(value) == true){
+      label.classList.remove("error");
+      icon.src = `./assets/icons/${iconPath}-default.png`;
+  }
+};
+// Funcion de validacion con 2 parametros:
+function validateFieldStylesParams(value1, value2, label, icon, iconPath, validationFunction){
+  if(validationFunction(value1, value2) == false){
+      label.classList.add('error');
+      icon.src = `./assets/icons/${iconPath}-error.png`;
+  } else if(validationFunction(value1, value2) == true){
+      label.classList.remove("error");
+      icon.src = `./assets/icons/${iconPath}-default.png`;
+  }
 };
 
-// Mostrar header y footer
-const displayHeaderAndFooter = () => {
-    header.style.display = 'grid';
-    footer.style.display = 'grid';
-};
+// Estilos de los inputs
+const signUpErrorFieldStyles = () => {
+  validateFieldStyles(signUpName.value, formLine_signUpName, signUp_nameIcon, 'user-icon', validateName);
+  validateFieldStyles(signUpPhone.value, formLine_signUpPhone, signUp_phoneIcon, 'phone-icon', validatePhone);
+  validateFieldStyles(signUpEmail.value, formLine_signUpEmail, signUp_emailIcon, 'email-icon', validateEmail);
+  validateFieldStyles(signUpPassword.value, formLine_signUpPassword, signUp_passwordIcon, 'password-icon', validatePassword);
 
-// Esconder header y footer
-const hideHeaderAndFooter = () => {
-    header.style.display = 'none';
-    footer.style.display = 'none';
-};
-
-// Mostrar Configuracion
-const displayConfig = () =>{
-  workflow.style.display = 'none';
-  sectionHome.style.display = 'none';
-  sectionConfig.style.display = 'grid';
+  validateFieldStylesParams(signUpPassword.value, signUpConfirmPassword.value, formLine_signUpConfirmPassword, signUp_confirmPasswordIcon, 'password-icon', validateConfirmPassword); 
 };
 
 
 
 
+// Validacion de informacion
+function saveUserData(){
+  if (validateName(signUpName.value) == true && validatePhone(signUpPhone.value) == true && validateEmail(signUpEmail.value) == true && validatePassword(signUpPassword.value) == true && validateConfirmPassword(signUpPassword.value, signUpConfirmPassword.value) == true){
+      localStorage.setItem('userName', signUpName.value);
+      localStorage.setItem('userPhone', signUpPhone.value);
+      localStorage.setItem('userEmail', signUpEmail.value);
+      localStorage.setItem('userPassword', signUpPassword.value);
+      // Guardo los datos del usuario
+      userOnStorage.userName = localStorage.getItem('userName');
+      userOnStorage.userEmail = localStorage.getItem('userEmail');
+      userOnStorage.userPhone = localStorage.getItem('userPhone');
+      userOnStorage.userPassword = localStorage.getItem('userPassword');
+
+      signUpName.value = "";
+      signUpPhone.value = "";
+      signUpEmail.value = "";
+      signUpPassword.value = "";
+      signUpConfirmPassword.value = "";
+
+      Toastify({
+          text: "It has been successfully registered",
+          backgroundColor: "rgba(37, 108, 41, 0.80)",
+          classes: "succes",
+      }).showToast();
+      
+      // HIDE
+      header.style.display = 'none';
+      footer.style.display = 'none';
+      sectionHome.style.display = 'none';
+      sectionSignUp.style.display = 'none';
+      // SHOW
+      sectionLogIn.style.display = 'grid';
+
+  } else {
+      signUpErrorFieldStyles();
+  };
+};
+// Validacion si el usuario ya existe
+function alreadyUserChecker(){
+  if (signUpEmail.value == localStorage.getItem('userEmail')){
+      swal({
+          title: "Ups",
+          text: "Email account is already registered, do you want to log in?",
+          icon: "warning",
+          buttons: ["Cancel", "Log in"],
+        })
+        .then((value) => {
+          if (value) {
+              // HIDE
+              header.style.display = 'none';
+              footer.style.display = 'none';
+              sectionHome.style.display = 'none';
+              sectionSignUp.style.display = 'none';
+              // SHOW
+              sectionLogIn.style.display = 'grid';
+          }
+        });
+  } else {
+      saveUserData();
+  };
+};
+
+// Funcion inicializadora
+function initSignUp(){
+  signUpform.addEventListener('submit', (e) => {
+      e.preventDefault();
+      alreadyUserChecker();
+  });
+};
 
 
+// LOG IN
+
+// Validacion de datos:
+
+// Validacion de Usuario, nombre/mail/numero
+function validateUserLogIn(){
+  if(logInUser.value == localStorage.getItem('userName') || logInUser.value == localStorage.getItem('userEmail') || logInUser.value == localStorage.getItem('userPhone')){
+      return true;
+  } else {
+      return false;
+  };
+};
+
+// Validacion de contrasenia
+function validatePasswordLogIn(){
+  if(logInPassword.value == localStorage.getItem('userPassword')){
+      return true;
+  } else {
+      return false;
+  };
+};
+
+// Estilos de los inputs
+const logInErrorFieldStyles = () => {
+  validateFieldStyles(logInUser.value, formLine_logInUser, logIn_userIcon, 'user-icon', validateUserLogIn);
+  validateFieldStyles(logInPassword.value, formLine_logInPassword, logIn_passwordIcon, 'password-icon', validatePasswordLogIn);
+};
+
+let tryToLogIn = 0;
+// Funcion inicializadora
+function initLogIn(){
+  logInForm.addEventListener('submit', (e) => {
+      e.preventDefault();
+
+      if(tryToLogIn < 3){
+          if(validateUserLogIn() == true && validatePasswordLogIn() == true){
+              tryToLogIn = 0;
+              sessionStorage.setItem('session', 'active');
+              location.reload();
+          } else {
+              logInErrorFieldStyles();
+              tryToLogIn++;
+          };
+      } else if (tryToLogIn >= 3 && tryToLogIn < 6) {
+          swal({
+              title: "Hey",
+              text: "Is it a brute force attack or have you just forgotten your password?",
+              icon: "warning",
+              button: "Continue",
+          });
+          if(validateUserLogIn() == true && validatePasswordLogIn() == true){
+              tryToLogIn = 0;
+              sessionStorage.setItem('session', 'active');
+              location.reload();
+          } else {
+              logInErrorFieldStyles();
+              tryToLogIn++;
+          };
+      } else if (tryToLogIn >= 6){
+          swal({
+              title: "Stop",
+              text: "First you must create an account and then log in.",
+              icon: "error",
+              button: "Continue",
+          });
+          if(validateUserLogIn() == true && validatePasswordLogIn() == true){
+              tryToLogIn = 0;
+              sessionStorage.setItem('session', 'active');
+              location.reload();
+          } else {
+              logInErrorFieldStyles();
+              tryToLogIn++;
+          };
+      };
+      
+  });
+};
 
 
 
